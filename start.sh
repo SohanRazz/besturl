8720635f0d240d6e13d43ec664d4536fabb6e919
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-02-26 14:37:48 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $SOURCE_CODE ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Ps705112/besturl /besturl
+else
+  echo "Cloning Custom Repo from $SOURCE_CODE "
+  git clone $SOURCE_CODE /besturl
+fi
+cd /besturl
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 main.py
